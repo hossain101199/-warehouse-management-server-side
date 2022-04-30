21e3d93f3f192478f146da41c945c37b67a5117d
hossain101199 <hossainbhai1011@gmail.com>
2022-05-01 04:19:00 +0600

    mongo connected

---
+++ README.md
@@ -0,0 +1,7 @@
+# Healthy Health Warehouse-server-side
+
+Website: [Healthy Health](https://healthy-health-ade6b.web.app).
+
+## Available Scripts
+
+In the project directory, you can run:
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+// ---------------------------------------------------------------------------
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dtctp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {}
+run().catch(console.dir);
+
+// ---------------------------------------------------------------------------
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+// ---------------------------------------------------------------------------
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
