86bda2825ceb1a9a81f93abedbad0554c5767ebd
hossain101199 <hossainbhai1011@gmail.com>
2022-05-08 22:15:27 +0600

    cors

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+// enable cors
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // ---------------------------------------------------------------------------
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dtctp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
